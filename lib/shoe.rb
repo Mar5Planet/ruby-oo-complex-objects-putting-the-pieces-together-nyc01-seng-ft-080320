eacaba21b809d7b2659bf9de9d59d0dbe9a4348f
Mar Mustafa <amustaf@email.arizona.edu>
2020-08-04 15:25:34 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -5,5 +5,9 @@ class Shoe
     @brand = brand
   end 
   
-  attr_accessor
+  attr_accessor :brand
+  attr_accessor :color
+  attr_accessor :size 
+  attr_accessor :material
+  attr_accessor :condition
 end 
